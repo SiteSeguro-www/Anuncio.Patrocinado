07691d123f4956aa094c5341016ecf77c0e8f52f
DevDW <dweminem@gmail.com>
2026-05-22 07:49:26 -0300

    build: inline assets into a single file
    Configure vite-plugin-singlefile to bundle the project into a single HTML file for easier distribution.

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig} from 'vite';
+import { viteSingleFile } from 'vite-plugin-singlefile';
 
 export default defineConfig(() => {
   return {
@@ -9,7 +10,7 @@ export default defineConfig(() => {
     build: {
       outDir: 'docs',
     },
-    plugins: [react(), tailwindcss()],
+    plugins: [react(), tailwindcss(), viteSingleFile()],
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
